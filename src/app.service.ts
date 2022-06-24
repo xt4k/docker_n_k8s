7c915bac0c551f28f8e75e448448ba0f099c793c
Laith Harb <laithharb@Laiths-iMac.local>
2022-06-24 07:35:42 -0400

    the power forward

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'IS IT A CPU THING!!!!!!';
+    return 'Is this goona work';
   }
 }
 // containerDefinitions.image
