450232329fd17c7de47f8db690876b86732a6b37
Laith Harb <laithharb@Laiths-iMac.local>
2022-06-24 07:45:16 -0400

    kooobe

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Is this goona work';
+    return 'The maginc 13';
   }
 }
 // containerDefinitions.image
