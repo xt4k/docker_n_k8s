e1dac55e2baef6913e323664f8b12930185edfe7
Laith Harb <laithharb@Laiths-iMac.local>
2022-06-23 22:53:20 -0400

    sadas

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Goodbye World!!!!!!';
+    return 'DOES IT WORK??';
   }
 }
 // containerDefinitions.image
