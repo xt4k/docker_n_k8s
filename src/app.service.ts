e73a09ce15b36de09f1870666ad76f4984e40c23
xt4k <yuriy.logvinov@gmail.com>
2023-08-11 16:28:15 +0300

    work with task definition-12

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Good-bye world';
+    return 'Good-bye world. again hope';
   }
 }
